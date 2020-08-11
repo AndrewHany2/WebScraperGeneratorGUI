d22517f6cab93c92a4b49a97e6096525dedf1dcc
joeeltoukhy <joseph.toukhy@gmail.com>
2020-08-12 00:03:50 +0200

    generate schema dropdown

--- src/components/Navbars/AdminNavbarLinks.js
+++ src/components/Navbars/AdminNavbarLinks.js
@@ -51,7 +51,7 @@ export default function AdminNavbarLinks() {
   };
 
   const handleCloseProfile = () => {
- 
+    setOpenProfile(null);
   };
 
   const logoutHandler = (event) => {
--- src/views/GenerateSchema/GenerateSchema.js
+++ src/views/GenerateSchema/GenerateSchema.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+import React, { Component, useEffect  } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import axios from "axios";
 import Radio from '@material-ui/core/Radio';
 import RadioGroup from '@material-ui/core/RadioGroup';
@@ -7,50 +8,134 @@ import FormControl from '@material-ui/core/FormControl';
 import FormHelperText from '@material-ui/core/FormHelperText';
 import FormLabel from '@material-ui/core/FormLabel';
 import Button from '@material-ui/core/Button';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemText from '@material-ui/core/ListItemText';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+import GridItem from "components/Grid/GridItem.js";
+import GridContainer from "components/Grid/GridContainer.js";
+import Card from "components/Card/Card.js";
+import CardHeader from "components/Card/CardHeader.js";
+import CardBody from "components/Card/CardBody.js";
 
 
-class GenerateSchema extends Component {
-	state = {
-		schemas: [],
-	}
+const useStyles = makeStyles((theme) => ({
+	root: {
+	  backgroundColor: theme.palette.background.paper,
+	},
+	cardCategoryWhite: {
+		color: "rgba(255,255,255,.62)",
+		margin: "0",
+		fontSize: "14px",
+		marginTop: "0",
+		marginBottom: "0",
+	  },
+	  cardTitleWhite: {
+		color: "#FFFFFF",
+		marginTop: "0px",
+		minHeight: "auto",
+		fontWeight: "300",
+		fontFamily: "'Roboto', 'Helvetica', 'Arial', sans-serif",
+		marginBottom: "3px",
+		textDecoration: "none",
+	  },
+  }));
 
-	componentWillMount = () => {
+const schemas = [];
+  
+const options = [];
+  
+  export default function GenerateSchema() {
+	const classes = useStyles();
+	const [anchorEl, setAnchorEl] = React.useState(null);
+	const [selectedIndex, setSelectedIndex] = React.useState(1);
+  
+	const handleClickListItem = (event) => {
+	  setAnchorEl(event.currentTarget);
+	};
+  
+	const handleMenuItemClick = (event, index) => {
+	  setSelectedIndex(index);
+	  setAnchorEl(null);
+	};
+  
+	const handleClose = () => {
+	  setAnchorEl(null);
+	};
+	
+	useEffect(() => {
 		const authToken = localStorage.getItem('AuthToken');
 		axios.defaults.headers.common = { Authorization: `${authToken}` };
 		axios
 			.get('/todos')
 			.then((response) => {
-				this.setState({
-					schemas: response.data,
-					uiLoading: false
-				});
+				// options.push(response.data);
+				var i;
+				for (i = 0; i < response.data.length ; i++) {
+					options.push(response.data[i].body.mainInfo.name);
+				  }
+				// console.log(response.data[0].body.mainInfo.name);
+				// console.log(response.data.length);
 			})
 			.catch((err) => {
 				console.log(err);
 			});
-	};
+		console.log(options);
 
-	handleRadioChange = (event) => {
-	};
+	},[]);
+  
+	return (
+	<GridContainer>
+      <GridItem xs={12} sm={12} md={12}>
+        <Card>
+          <CardHeader color="primary">
+            <h4 className={classes.cardTitleWhite}>Generate Schema</h4>
+            <p className={classes.cardCategoryWhite}>Choose your schema</p>
 
-	render() {
-		return (
-			<form /*onSubmit={handleSubmit}*/>
-				<FormControl component="fieldset" >
-					<FormLabel component="legend">Choose your schema</FormLabel>
-					<RadioGroup aria-label="quiz" name="quiz" /*onChange={handleRadioChange}*/>
-						{
-							this.state.schemas.map((schema =>
-								<FormControlLabel value="best" control={<Radio />} label={schema.mainInfo.name} />))
-						}
-					</RadioGroup>
-					<Button type="submit" variant="outlined" color="primary">
-						Generate Schema
-			  </Button>
-				</FormControl>
-			</form>
-		);
-	}
-}
-
-export default GenerateSchema;
+          </CardHeader>
+          <CardBody>
+		  <GridContainer>
+                  <GridItem xs={12} sm={12} md={4}>
+                    <h4>Choose your schema</h4>
+                  </GridItem>
+                  <GridItem xs={12} sm={12} md={4}>
+				  <List component="nav" aria-label="Device settings">
+		  <ListItem
+			button
+			aria-haspopup="true"
+			aria-controls="lock-menu"
+			aria-label="when device is locked"
+			onClick={handleClickListItem}
+		  >
+			<ListItemText primary="When device is locked" secondary={options[selectedIndex]} />
+		  </ListItem>
+		</List>
+		<Menu
+		  id="lock-menu"
+		  anchorEl={anchorEl}
+		  keepMounted
+		  open={Boolean(anchorEl)}
+		  onClose={handleClose}
+		>
+		  {options.map((option, index) => (
+			<MenuItem
+			  key={option}
+			  disabled={index === 0}
+			  selected={index === selectedIndex}
+			  onClick={(event) => handleMenuItemClick(event, index)}
+			>
+			  {option}
+			</MenuItem>
+		  ))}
+		</Menu>
+                  </GridItem>
+                </GridContainer>
+		
+          </CardBody>
+        </Card>
+      </GridItem>
+    </GridContainer>
+	  
+	);
+  }
